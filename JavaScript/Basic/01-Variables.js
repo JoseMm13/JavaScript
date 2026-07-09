523e3e4a6fdfa28f8e75570b2c81efffe96a830d
JoseMm13 <jagmns43@gmail.com>
2026-07-09 10:56:37 +0200

    Adding Variables.js

---
+++ JavaScript/Basic/01-Variables.js
@@ -0,0 +1,34 @@
+//Tipos de variables en JavaScript
+
+// A la hora de declarar variables en JavaScript, debes tener en cuenta el lowerCamelCase, 
+// que es la convención de nomenclatura que se utiliza en JavaScript para nombrar variables y funciones.
+
+// Tipo var 
+
+var helloWorld = "Hello, welcome to JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo, bienvenido a JavaScript!"
+console.log(helloWorld)
+
+
+// Tipo let
+
+let helloWorld2 = "Hello, welcome to JavaScript!"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo, bienvenido a JavaScript!"
+console.log(helloWorld2)
+
+// Tipo const
+
+const helloWorldConst = "Hello, welcome to JavaScript!"
+console.log(helloWorldConst)
+
+
+//Nota:  No puedes reasignar una propiedad que en este caso es constante.
+// En este caso si intentas reasignarlo te dara un error.
+/*
+    helloWorldConst = "Hola de nuevo, bienvenido a JavaScript!"
+    console.log(helloWorldConst) 
+*/
