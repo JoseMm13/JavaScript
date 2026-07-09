96e1421e95c00bb586d9cb514407d9ab154101fc
JoseMm13 <jagmns43@gmail.com>
2026-07-09 19:14:13 +0200

    Adding Error-Handling.js

---
+++ JavaScript/Basic/24-Error-Handling.js
@@ -0,0 +1,21 @@
+// Excepcitions
+
+// Produce una excepción
+let myObject
+// console.log(myObject.email)
+
+// Captura de errores
+
+// try-catch
+
+try {
+    // Código que intenta ejecutar
+    console.log(myObject.email)
+    console.log("Finaliza la ejecución sin errores")
+} catch {
+    // Bloque de error
+    console.log("Se ha producido un error")
+}
+
+// Captura del error
+
