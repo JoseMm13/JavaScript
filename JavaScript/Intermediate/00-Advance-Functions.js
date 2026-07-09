06c5191d41e482799f3141ae949b78bb9cfef01c
JoseMm13 <jagmns43@gmail.com>
2026-07-09 20:59:19 +0200

    Adding Advance-Functions.js

---
+++ JavaScript/Intermediate/00-Advance-Functions.js
@@ -0,0 +1,41 @@
+//First-class citizens.
+
+const greet = function(name) {
+    console.log(`Hello, ${name}`)
+}
+
+greet("JMELTY")
+
+function processGreeting(greetfunction, name){
+    greetfunction(name)
+}
+
+function returnGreeting(){
+    return greet
+}
+
+processGreeting(greet,"JMELTY")
+const greet2 = returnGreeting()
+greet2("Jmelty (LEARN IT)")
+
+// Advanced arrow functions.
+
+// -Implicit return.
+
+const multiply = (a, b) => a * b
+console.log(multiply(13,21))
+
+// this Lexical / Tokens
+
+const handler = {
+    name: "Jmelty",
+    greeting: function () {
+        console.log(`Hello, ${this.name}`)
+    },
+    arrowGreeting: () => {
+        console.log(`Hello, ${this.name}`)
+    }
+}
+
+handler.greeting()
+handler.arrowGreeting();
