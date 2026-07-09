e97de735c81c3f4f82d36c6dbdc89c73fd0ead57
JoseMm13 <jagmns43@gmail.com>
2026-07-09 19:43:13 +0200

    Adding Error-Handling.js

--- JavaScript/Basic/24-Error-Handling.js
+++ JavaScript/Basic/24-Error-Handling.js
@@ -19,3 +19,86 @@ try {
 
 // Captura del error
 
+try{
+    console.log(myObject.email)
+}catch(error){
+    console.log("Error ", error.message)
+}
+
+// Finally
+
+try{
+    console.log(myObject.email)
+}catch(error){
+    console.log("Error ", error.message)
+}finally{
+    console.log("This code always runs.")
+}
+
+// No está soportado
+// try {
+//     console.log(myObject.email)
+// } finally {
+//     console.log("Este código se ejecuta siempre")
+// }
+
+// Lanzamiento de errores
+
+// throw
+
+// throw new Error("Error")
+
+function sumIntegers(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        throw new TypeError("These properties cannot be summed.")
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        throw new Error("This operation only sums integers.")
+    }
+    if (a == 0 || b == 0) {
+        throw new SumZeroIntegerError("An attempt is being made to add zero.", a, b)
+    }
+    return a + b
+}
+
+try {
+    console.log(sumIntegers(5, 10))
+    // console.log(sumIntegers(5.5, 10))
+    console.log(sumIntegers("5", 10))
+    // console.log(sumIntegers(5, "10"))
+    // console.log(sumIntegers("5", "10"))
+} catch (error) {
+    console.log("Error:", error.message)
+}
+
+// Capturar varios tipos de errores
+
+try {
+    // console.log(sumIntegers(5.5, 10))
+    console.log(sumIntegers("5", 10))
+} catch (error) {
+    if (error instanceof TypeError) {
+        console.log("TypeError:", error.message)
+    } else if (error instanceof Error) {
+        console.log("Error:", error.message)
+    }
+}
+
+class SumZeroIntegerError extends Error {
+    constructor(message, a, b) {
+        super(message)
+        this.a = a
+        this.b = b
+    }
+
+    printNumbers() {
+        console.log(this.a, " + ", this.b)
+    }
+}
+
+try {
+    console.log(sumIntegers(0, 10))
+} catch (error) {
+    console.log("Custom Error:", error.message)
+    error.printNumbers()
+}
