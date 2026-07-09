2e54caa80321b4207b784a612b0478a0b999d981
JoseMm13 <jagmns43@gmail.com>
2026-07-09 10:40:12 +0200

    Primer commit

---
+++ JavaScript/Basic/00-Hello-World.js
@@ -0,0 +1,28 @@
+console.log("Welcome Developers, This is JavaScript Basic Tutorial")
+//Formas de escribir en JavaScript
+
+//1 Usando comillas dobles
+console.log("Hello World");
+//2 Usando comillas simples
+console.log('Hello World');
+//3 Usando comillas invertidas (template literals)
+console.log(`Hello World`);
+
+
+//Formas de escribir comentarios en JavaScript
+
+// La doble barra (//) se utiliza para escribir comentarios de una sola línea en JavaScript. 
+// Todo lo que sigue a las dos barras se considera un comentario y no se ejecutará como código.
+
+/*  La barra y el asterisco (/*) se utilizan para escribir comentarios de varias líneas en JavaScript. 
+    Todo lo que esté entre /* y * / se considera un comentario y no se ejecutará como código.
+*/
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2) //Cinco elevado a dos.
