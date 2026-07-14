9f960f02cbd18b4af1d9d188d79bd11a17cf4956
JoseMm13 <jagmns43@gmail.com>
2026-07-14 19:27:24 +0200

    Adding Advance-Functions-Exercises.js (NO SOLUTION)

---
+++ JavaScript/Intermediate/01-Advanced-Functions-Exercises.js
@@ -0,0 +1,19 @@
+// 1. Create a function that returns another function
+
+// 2. Implement a curried function that multiplies 3 numbers
+
+// 3. Develop a recursive function that calculates the power of a number raised to an exponent
+
+// 4. Create a createCounter() function that receives an initial value and returns an object with methods for increment(), decrement() and getValue(), using a closure to maintain the state
+
+// 5. Create a sumManyTimes(multiplier, ...numbers) function that first sums all the numbers (using Rest parameters) and then multiplies the result by multiplier
+
+// 6. Create a Callback that is invoked with the result of the sum of all the numbers passed to a function
+
+// 7. Develop a partial function
+
+// 8. Implement an example that makes use of Spread
+
+// 9. Implement an implicit return
+
+// 10. Make use of lexical (this)
